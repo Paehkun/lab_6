2c164fd623be171313d1672c47d60a8a02a4d255
Mohamad Faiz Bin Rosdin <faizrosdin0123@gmail.com>
2024-05-19 22:17:48 +0800

    code

---
+++ lab6_q1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>Lab 6 Q1</title>
+</head>
+<body>
+ <?php
+ $name = "Mohamad Faiz";
+ $matricNo = "DI220107";
+ $course  = "Information Technology";
+ $year = "2";
+ $address = "No 71 , Jalan Maryani, Kampung Jawa, 85000 Segamat, Johor ";
+ ?>
+ <table>
+ <tr>
+ <td>Name</td>
+ <td><?php echo "$name"; ?></td>
+ </tr>
+
+ <tr>
+ <td>Matric Number</td>
+ <td><?php echo "$matricNo"; ?></td>
+ </tr>
+ 
+
+ <tr>
+ <td>Course</td>
+ <td><?php echo "$course"; ?></td>
+ </tr>
+
+ <tr>
+ <td>Year of Study</td>
+ <td><?php echo "$year"; ?></td>
+ </tr>
+
+ <tr>
+ <td>Address</td>
+ <td><?php echo "$address"; ?></td>
+ </tr>
+ </table>
+
+</body>
+</html>
---
+++ lab6_q3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 6 Q3</title>
+</head>
+<body>
+    <?php
+    $width1 = 4;
+    $width2 = 2;
+    $area = $width1 * $width2; 
+    ?>  
+    <?php echo "The area of rectangle with a width of $width1 and $width2 is $area " ?>  
+
+</body>
+</html>
